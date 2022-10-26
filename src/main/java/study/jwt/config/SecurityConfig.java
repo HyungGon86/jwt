37c6881cda9dd27d9873b667e7f46d98db0af25c
HyungGon <kgh198621@naver.com>
2022-10-27 01:04:47 +0900

    Initial commit

---
+++ src/main/java/study/jwt/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package study.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(corsFilter)
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .hasAnyRole("USER", "MANAGER", "ADMIN")
+                .antMatchers("/api/v1/manager/**")
+                .hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers("/api/v1/admin/**")
+                .hasRole("ADMIN")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
